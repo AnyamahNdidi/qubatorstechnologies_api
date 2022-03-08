764492687f5a7ce43298adc60466b394967a0e8d
AnyamahNdidi <theo4felix@gmail.com>
2022-03-08 06:35:59 +0100

    api for cors

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ const app = express()
 // define a port
 const PORT = process.env.PORT || 2022
 
+const cors = require('cors')
+
 // use express middleware to posting of date
 app.use(express.json())
 // create a default route
+app.use(cors())
 app.use("/api", require("./router/router"))
 // connect the server to a port
 app.listen(PORT, ()=>{
